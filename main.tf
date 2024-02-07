5c1dad6f6f9044d2b70de67dff199248249d270c
Grant Orchard <go@hashicorp.com>
2024-02-07 14:57:09 +1100

    refactoring

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-locals {
-  github_usernames = flatten(concat(
-    [ for team in data.github_organization_teams.team_se.teams: team.members if team.name == "team-se" ],
-    [ for team in data.github_organization_teams.team_se.teams: team.members if team.name == "apac-se" ]
-  ))
-}
-
 # Lookup our GitHub org for teams and memberships
 data "github_team" "this" {
   for_each = toset(var.github_teams)
